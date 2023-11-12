613dbb34ac9e8dce5ab46302122196c03e87f6ce
zsaritek <zsaritek@gmail.com>
2023-11-13 00:34:21 +0100

    hbs

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const isLoggedIn = require("../middleware/isLoggedIn");
 /* GET home page */
-router.get("/", isLoggedIn, async (req, res, next) => {
-  res.render('index');
+router.get("/", /*isLoggedIn,*/ async (req, res, next) => {
+  const currentUser = req.session.currentUser
+  res.render('index', { currentUser });
 });
 
 module.exports = router;
