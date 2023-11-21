1f89b4a141203bab867b93b45bf40caca383fb34
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2023-11-21 20:54:04 +0100

    sign in sign up error hbs

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
     // this middleware runs whenever requested page is not available
-    res.status(404).render("not-found");
+    res.status(404).render("error");
   });
 
   app.use((err, req, res, next) => {
